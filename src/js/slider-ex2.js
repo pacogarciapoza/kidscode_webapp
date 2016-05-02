bea5fa709c83b106edf04679bd25583734205b58
Silvia J Boo <sljboo@gmail.com>
2016-05-02 20:43:24 +0200

    modified first slide > prev slide to go to index.html

--- src/js/slider-ex2.js
+++ src/js/slider-ex2.js
@@ -66,7 +66,12 @@ $(document).ready(function() {
 		var prevSlide = actualSlide.prev();
 		
 		if (prevSlide.length == 0) {
-			prevSlide = $('#slide-2-8');//cambiarlo a página de selección de ejercicios
+			//prevSlide = $('#slide-2-8');//cambiarlo a página de selección de ejercicios
+			//vamos a la página de selección de ejercicio index.html
+			$(".container-nav-btn-cancel").click(function() { 
+     			location.href = this.href; 
+			});
+			$(".container-nav-btn-cancel").click();
 		}
 
 		if (actualSlide.data('slide')=='1'|| actualSlide.data('slide')=='2'||actualSlide.data('slide')=='3'||actualSlide.data('slide')=='4' || actualSlide.data('slide')=='5' || actualSlide.data('slide')=='6')
