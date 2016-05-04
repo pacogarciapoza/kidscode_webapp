94f63b9af7375baefaa0dec1e4f815f4378b2681
Silvia J Boo <sljboo@gmail.com>
2016-05-05 00:19:13 +0200

    Update 2016-05-04T22:19:11.989Z

--- js/orientation.js
+++ js/orientation.js
@@ -1,115 +1,124 @@
 //detectamos orientation del móvil para informar que el juego funciona
 //solo en landscape
 
+function isDesktop(){
+	if ($(window).width()>1020) {
+		return true;
+	}
+}
+
 //cuando se carga la página
 $(document).ready(function() {
-	//iOS
-	switch (window.orientation) {  
-    case 0:  
-        // Portrait 
-        if ($('.container-alert').hasClass('closed'))
-        {
-    	    $('.container-alert').removeClass('closed');
-    	}
-        break; 
-        
-    case 180:  
-        // Portrait (Upside-down)
-        if ($('.container-alert').hasClass('closed'))
-        {
-    	    $('.container-alert').removeClass('closed');
-    	}
-        break; 
-    
-    default: //90 and -90 are landscape
-    	if (!($('.container-alert').hasClass('closed')))
-        {
-			$('.container-alert').addClass('closed');
-		}
-		break;
-	}
+	if(!isDesktop()){
+		//iOS
+		switch (window.orientation) {  
+			case 0:  
+	        // Portrait 
+	        if ($('.container-alert').hasClass('closed'))
+	        {
+	        	$('.container-alert').removeClass('closed');
+	        }
+	        break; 
+	        
+	        case 180:  
+	        // Portrait (Upside-down)
+	        if ($('.container-alert').hasClass('closed'))
+	        {
+	        	$('.container-alert').removeClass('closed');
+	        }
+	        break; 
 
-	//android
-	switch (window.screen.orientation.angle) {  
-    case 0:  
-        // Portrait
-        if ($('.container-alert').hasClass('closed'))
-        { 
-        	$('.container-alert').removeClass('closed');
-        }
-        break; 
-        
-    case 180:  
-        // Portrait (Upside-down)
-        if ($('.container-alert').hasClass('closed'))
-        { 
-        	$('.container-alert').removeClass('closed');
-        }
-        break; 
-    
-    default:
-    	if (!($('.container-alert').hasClass('closed')))
-        {
-			$('.container-alert').addClass('closed');
+		    default: //90 and -90 are landscape
+		    if (!($('.container-alert').hasClass('closed')))
+		    {
+		    	$('.container-alert').addClass('closed');
+		    }
+		    break;
 		}
-		break;
+
+		//android
+		switch (window.screen.orientation.angle) {  
+			case 0:  
+	        // Portrait
+	        if ($('.container-alert').hasClass('closed'))
+	        { 
+	        	$('.container-alert').removeClass('closed');
+	        }
+	        break; 
+	        
+	        case 180:  
+	        // Portrait (Upside-down)
+	        if ($('.container-alert').hasClass('closed'))
+	        { 
+	        	$('.container-alert').removeClass('closed');
+	        }
+	        break; 
+
+	        default:
+	        if (!($('.container-alert').hasClass('closed')))
+	        {
+	        	$('.container-alert').addClass('closed');
+	        }
+	        break;
+	    }
 	}
 });
 
 //cuando se realiza un cambio de orientation del dispositivo
 $(window).bind("orientationchange",function(){
-	//iOS
-	switch (window.orientation) {  
-    case 0:  
-        // Portrait 
-        if ($('.container-alert').hasClass('closed'))
-        { 
-        	$('.container-alert').removeClass('closed');
-        }
-        break; 
-        
-    case 180:  
-        // Portrait  (Upside-down)
-        if ($('.container-alert').hasClass('closed'))
-        { 
-        	$('.container-alert').removeClass('closed');
-        }
-        break;  
-    
-    default:
-    	if (!($('.container-alert').hasClass('closed')))
-        {
-			$('.container-alert').addClass('closed');
-		}
-		break;
-	}
+	if(!isDesktop()){
+		//iOS
+		switch (window.orientation) {  
+			case 0:  
+	        // Portrait 
+	        if ($('.container-alert').hasClass('closed'))
+	        { 
+	        	$('.container-alert').removeClass('closed');
+	        }
+	        break; 
+	        
+	        case 180:  
+	        // Portrait  (Upside-down)
+	        if ($('.container-alert').hasClass('closed'))
+	        { 
+	        	$('.container-alert').removeClass('closed');
+	        }
+	        break;  
 
-	//android
-	switch (window.screen.orientation.angle) {  
-    case 0:  
-        // Portrait 
-        if ($('.container-alert').hasClass('closed'))
-        { 
-        	$('.container-alert').removeClass('closed');
-        }
-        break; 
-        
-    case 180:  
-        // Portrait  (Upside-down)
-        if ($('.container-alert').hasClass('closed'))
-        { 
-        	$('.container-alert').removeClass('closed');
-        }
-        break; 
-    
-    default:
-    	if (!($('.container-alert').hasClass('closed')))
-        {
-			$('.container-alert').addClass('closed');
-		}
-		break;
-	}
+	        default:
+	        if (!($('.container-alert').hasClass('closed')))
+	        {
+	        	$('.container-alert').addClass('closed');
+	        }
+	        break;
+	    }
 
+		//android
+		switch (window.screen.orientation.angle) {  
+			case 0:  
+	        // Portrait 
+	        if ($('.container-alert').hasClass('closed'))
+	        { 
+	        	$('.container-alert').removeClass('closed');
+	        }
+	        break; 
+	        
+	        case 180:  
+	        // Portrait  (Upside-down)
+	        if ($('.container-alert').hasClass('closed'))
+	        { 
+	        	$('.container-alert').removeClass('closed');
+	        }
+	        break; 
+
+	        default:
+	        if (!($('.container-alert').hasClass('closed')))
+	        {
+	        	$('.container-alert').addClass('closed');
+	        }
+	        break;
+	    }
+	}
 	// if (window.orientation == 0 || window.orientation == 180)
 	// {
 	// 	$('.container-alert').removeClass('container-alert.closed');
