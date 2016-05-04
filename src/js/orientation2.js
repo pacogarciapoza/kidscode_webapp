fecd2ab2cb48b8ee87f36b88cf011d3303702372
Silvia J Boo <sljboo@gmail.com>
2016-05-05 00:18:51 +0200

    added jquery that validates orientation and show msg if is portrait

---
+++ src/js/orientation2.js
@@ -0,0 +1,123 @@
+//detectamos orientation del móvil para informar que el juego funciona
+//solo en landscape
+
+function isDesktop(){
+	if ($(window).width()>1020) {
+		return true;
+	}
+}
+
+//cuando se carga la página
+$(document).ready(function() {
+	if(!isDesktop()){
+		//iOS
+		switch (window.orientation) {  
+			case 0:  
+	        // Portrait 
+	        if ($('.container-alert2').hasClass('closed'))
+	        {
+	        	$('.container-alert2').removeClass('closed');
+	        }
+	        break; 
+	        
+	        case 180:  
+	        // Portrait (Upside-down)
+	        if ($('.container-alert2').hasClass('closed'))
+	        {
+	        	$('.container-alert2').removeClass('closed');
+	        }
+	        break; 
+
+		    default: //90 and -90 are landscape
+		    if (!($('.container-alert2').hasClass('closed')))
+		    {
+		    	$('.container-alert2').addClass('closed');
+		    }
+		    break;
+		}
+
+		//android
+		switch (window.screen.orientation.angle) {  
+			case 0:  
+	        // Portrait
+	        if ($('.container-alert2').hasClass('closed'))
+	        { 
+	        	$('.container-alert2').removeClass('closed');
+	        }
+	        break; 
+	        
+	        case 180:  
+	        // Portrait (Upside-down)
+	        if ($('.container-alert2').hasClass('closed'))
+	        { 
+	        	$('.container-alert2').removeClass('closed');
+	        }
+	        break; 
+
+	        default:
+	        if (!($('.container-alert2').hasClass('closed')))
+	        {
+	        	$('.container-alert2').addClass('closed');
+	        }
+	        break;
+	    }
+	}
+});
+
+//cuando se realiza un cambio de orientation del dispositivo
+$(window).bind("orientationchange",function(){
+	if(!isDesktop()){
+		//iOS
+		switch (window.orientation) {  
+			case 0:  
+	        // Portrait 
+	        if ($('.container-alert2').hasClass('closed'))
+	        { 
+	        	$('.container-alert2').removeClass('closed');
+	        }
+	        break; 
+	        
+	        case 180:  
+	        // Portrait  (Upside-down)
+	        if ($('.container-alert2').hasClass('closed'))
+	        { 
+	        	$('.container-alert2').removeClass('closed');
+	        }
+	        break;  
+
+	        default:
+	        if (!($('.container-alert2').hasClass('closed')))
+	        {
+	        	$('.container-alert2').addClass('closed');
+	        }
+	        break;
+	    }
+
+		//android
+		switch (window.screen.orientation.angle) {  
+			case 0:  
+	        // Portrait 
+	        if ($('.container-alert2').hasClass('closed'))
+	        { 
+	        	$('.container-alert2').removeClass('closed');
+	        }
+	        break; 
+	        
+	        case 180:  
+	        // Portrait  (Upside-down)
+	        if ($('.container-alert2').hasClass('closed'))
+	        { 
+	        	$('.container-alert2').removeClass('closed');
+	        }
+	        break; 
+
+	        default:
+	        if (!($('.container-alert2').hasClass('closed')))
+	        {
+	        	$('.container-alert2').addClass('closed');
+	        }
+	        break;
+	    }
+	}
+
+});
